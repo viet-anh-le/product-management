8a97a6edb6b1f38142a0bf05945a25cbfcb84a5f
Viet Anh <vietanhle3012bn@gmail.com>
2025-02-26 17:03:51 +0700

    Đẩy file tĩnh lên cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const multer = require("multer");
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
 
-const upload = multer({storage: storageMulter()});
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller")
 const validate = require("../../validates/admin/product.validate")
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get('/', controller.index);
 
@@ -18,14 +19,20 @@ router.delete('/delete/:id', controller.deleteItem);
 
 router.get('/create', controller.create);
 
-router.post('/create', upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.post('/create',
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
 
 router.get('/edit/:id', controller.edit);
 
-router.patch('/edit/:id', 
-  upload.single("thumbnail"), 
+router.patch('/edit/:id',
+  upload.single("thumbnail"),
   validate.createPost,
-  controller.editPatch);
+  controller.editPatch
+);
 
 router.get('/detail/:id', controller.detail);
 

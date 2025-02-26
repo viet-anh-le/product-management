095bf0579cea733539dea26afa9dfdfb6c12afe7
Viet Anh <vietanhle3012bn@gmail.com>
2025-02-26 17:21:02 +0700

    fix delete func

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -98,7 +98,7 @@ module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
   
   // await Product.deleteOne({_id: id});
-  await Product.updateOne({_id: id, deleted: true, deletedAt: new Date()});
+  await Product.updateOne({_id: id}, {deleted: true, deletedAt: new Date()});
 
   req.flash("success", `Đã xoá thành công`)
 

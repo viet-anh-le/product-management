2918066a88c7cbddc8426aa8d30a3d918fc95513
Viet Anh <vietanhle3012bn@gmail.com>
2025-02-26 20:00:09 +0700

    sắp xếp theo nhiều tiêu chí

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -36,8 +36,18 @@ module.exports.index = async (req, res) => {
 
   //End Pagination
 
+  //Sort
+  let sort = {};
+
+  if (req.query.sortKey && req.query.sortValue){
+    sort[req.query.sortKey] = req.query.sortValue
+  } else{
+    sort.position = "desc";
+  }
+  //End Sort
+
   const products = await Product.find(find)
-  .sort({position: "desc"})
+  .sort(sort)
   .limit(objectPagination.limitItems)
   .skip(objectPagination.skip);
 
@@ -157,10 +167,6 @@ module.exports.editPatch = async(req, res) => {
   req.body.discountPercentage = parseInt(req.body.discountPercentage);
   req.body.stock = parseInt(req.body.stock);
   req.body.position = parseInt(req.body.position);
-  
-  if (req.file){
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
-  }
   
   try {
     await Product.updateOne({
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -164,3 +164,39 @@ if (uploadImage){
   })
 }
 // End Upload Image
+
+//Sort
+const sort = document.querySelector("[sort]");
+if (sort){
+  let url = new URL(window.location.href);
+
+  const sortSelect = sort.querySelector("[sort-select]");
+  const sortClear = sort.querySelector("[sort-clear]");
+  sortSelect.addEventListener("change", (e) => {
+    const value = e.target.value;
+    const [sortKey, sortValue] = value.split("-");
+    
+    url.searchParams.set("sortKey", sortKey);
+    url.searchParams.set("sortValue", sortValue);
+
+    window.location.href = url.href;
+  })
+
+  sortClear.addEventListener("click", () => {
+    url.searchParams.delete("sortKey");
+    url.searchParams.delete("sortValue");
+
+    window.location.href = url.href;
+  })
+
+  //Theem selected cho option
+  const sortKey = url.searchParams.get("sortKey");
+  const sortValue = url.searchParams.get("sortValue");
+  if(sortKey && sortValue){
+    const stringSort = `${sortKey}-${sortValue}`;
+
+    const optionSelected = sortSelect.querySelector(`option[value='${stringSort}']`);
+    optionSelected.selected = true;
+  }
+}
+//End Sort

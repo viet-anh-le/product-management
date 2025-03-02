ea9b06671448a9d319d0c5051ad3dc7f057d7161
Viet Anh <vietanhle3012bn@gmail.com>
2025-03-03 05:43:38 +0700

    Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -48,7 +48,7 @@ module.exports.createPost = async (req, res) => {
 
   if (emailExist){
     req.flash("error", `Email ${req.body.email} đã tồn tại`);
-    req.redirect("back");
+    res.redirect("back");
   }
   else{
     req.body.password = md5(req.body.password);
@@ -56,4 +56,57 @@ module.exports.createPost = async (req, res) => {
     await record.save();
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
+}
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  let find = {
+    _id: req.params.id,
+    deleted: false 
+  };
+
+  try {
+    const data = await Account.findOne(find);
+    
+    const roles = await Role.find({
+      deleted: false
+    })
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles 
+    });
+
+  } catch(error){
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  const emailExist = await Account.findOne({
+    _id: {$ne: id},
+    email: req.body.email,
+    deleted: false
+  });
+  
+  if (emailExist){
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
+  }
+  else{
+    if (req.body.password){
+      req.body.password = md5(req.body.password);
+    }
+    else{
+      delete req.body.password;
+    }
+  
+    await Account.updateOne({_id: id}, req.body);
+  
+    req.flash("success", "Cập nhật tài khoản thành công");
+  }
+  res.redirect("back");
 }
--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -18,3 +18,18 @@ module.exports.createPost = (req, res, next) => {
   }
   next();
 }
+
+module.exports.editPatch = (req, res, next) => {
+  if (!req.body.fullName){
+    req.flash("error", "vui lòng nhập họ tên!");
+    res.redirect("back");
+    return;
+  }
+
+  if (!req.body.email){
+    req.flash("error", "vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+  next();
+}

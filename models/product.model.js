7b605d7626081fc9cf65cb065bd39de7af6685ae
Viet Anh <vietanhle3012bn@gmail.com>
2025-03-02 13:34:26 +0700

    Thêm mục chọn danh mục

--- models/product.model.js
+++ models/product.model.js
@@ -6,6 +6,10 @@ mongoose.plugin(slug);
 const productSchema = new mongoose.Schema(
   {
     title: String,
+    product_category_id: {
+      type: String,
+      default: ""
+    },
     description: String,
     price: Number,
     discountPercentage: Number,

5d9062dd6453d8a02b0b2b21907da385de8a55a5
Viet Anh <vietanhle3012bn@gmail.com>
2025-03-03 17:49:27 +0700

    Làm tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const productRoutes = require("./product.route")
 const productCategoryRoutes = require("./product-category.route")
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route")
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -18,4 +19,6 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/roles", roleRoutes);
 
   app.use(PATH_ADMIN + "/accounts", accountRoutes);
+
+  app.use(PATH_ADMIN + "/auth", authRoutes);
 }
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,14 @@
+module.exports.loginPost = (req, res, next) => {
+  if (!req.body.email){
+    req.flash("error", "vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+
+  if (!req.body.password){
+    req.flash("error", "vui lòng nhập mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+  next();
+}

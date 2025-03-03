d8339b9d5dc005c70a66eb59452166dba4117dbf
Viet Anh <vietanhle3012bn@gmail.com>
2025-03-04 06:52:06 +0700

    Thêm private route

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -5,9 +5,14 @@ const systemConfig = require("../../config/system");
 
 // [GET] /admin/auth/login
 module.exports.login = (req, res) => {
-  res.render("admin/pages/auth/login", {
-    pageTitle: "Trang đăng nhập"
-  });
+  if(req.cookies.token){
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+  }
+  else{
+    res.render("admin/pages/auth/login", {
+      pageTitle: "Trang đăng nhập"
+    });
+  }
 }
 
 // [POST] /admin/auth/login
@@ -42,9 +47,9 @@ module.exports.loginPost = async (req, res) => {
   res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 }
 
-// [GET] /admin/auth/login
+// [GET] /admin/auth/logout
 module.exports.logout = (req, res) => {
   //Xoá token trong cookie
   res.clearCookie("token");
-  res.redirect(`${systemConfig.prefixAdmin}/login`)
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
 }
--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require("../../controllers/admin/auth.controller");
-const validate = require("../../validates/admin/auth.validate")
+const validate = require("../../validates/admin/auth.validate");
 
 router.get('/login', controller.login);
 
